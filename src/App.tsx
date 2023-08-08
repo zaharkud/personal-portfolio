b5746b3457b05985698028bf1c409e82abadb993
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-08-09 00:01:58 +0300

    First commit. Layout structure completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./app.module.scss";
+import About from "./pages/about/about";
+import { useRef, useEffect, useState } from "react";
 
 function App() {
+  const sidebar = useRef<HTMLElement>(null);
+  const [pageOffset, setPageOffset] = useState<number>();
+
+  useEffect(() => {
+    setPageOffset(sidebar.current?.getBoundingClientRect().top);
+  }, [sidebar]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <section className={styles.sidebar} ref={sidebar}>
+              <section className={styles.info}>
+                <div className={styles.logo}></div>
+                <h2 className={styles.title}>Junior Frontend Developer</h2>
+                <p className={styles.description}>
+                  Начинающий веб-разработчик. Владею навыками проектирования
+                  интерфейсов, вёрстки и работы с backend.
+                </p>
+              </section>
+              <nav>
+                <ul className={styles.menu}>
+                  <li>
+                    <a>Обо мне</a>
+                  </li>
+                  <li>
+                    <a>Навыки</a>
+                  </li>
+                  <li>
+                    <a>Проекты</a>
+                  </li>
+                  <li>
+                    <a>Контакты</a>
+                  </li>
+                </ul>
+              </nav>
+            </section>
+            <section className={styles.page} style={{ marginTop: pageOffset }}>
+              <About />
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
