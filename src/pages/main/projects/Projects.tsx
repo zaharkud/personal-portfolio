fb853d296ac9e8a5918c60d922556ae6fdfbafb3
Zahar Kudriavcev <zaharkud162@gmail.com>
2023-09-10 16:35:30 +0300

    Added 2d project

--- src/pages/main/projects/Projects.tsx
+++ src/pages/main/projects/Projects.tsx
@@ -1,5 +1,6 @@
 import styles from "./projects.module.scss";
-import projectImg from "assets/img/projects/project__01-01.webp";
+import projectImg__01 from "assets/img/projects/project__01-01.webp";
+import projectImg__02 from "assets/img/projects/project__02-01.webp";
 import ProjectPromo from "components/ProjectPromo/ProjectPromo";
 import { Link } from "react-router-dom";
 
@@ -7,7 +8,7 @@ export default function Skills() {
   return (
     <section className={styles.container}>
       <article className={styles.project}>
-        <ProjectPromo projectImg={projectImg} projectLink="/project_01" />
+        <ProjectPromo projectImg={projectImg__01} projectLink="/project_01" />
         <div className={styles.project__bottom}>
           <div className={styles.techStack}>
             <article className={styles.techStack__item}>HTML</article>
@@ -18,13 +19,13 @@ export default function Skills() {
             <h3 className={styles.title}>Сайт-визитка строительной бригады</h3>
           </Link>
           <p className={styles.description}>
-            Начинающий веб-разработчик. Владею навыками проектирования
-            интерфейсов, вёрстки и работы с backend.
+            Информационный сайт с контактами, ценами на услуги и портфолио
+            выполненных работ
           </p>
         </div>
       </article>
       <article className={styles.project}>
-        <ProjectPromo projectImg={projectImg} projectLink="сайт-визитка" />
+        <ProjectPromo projectImg={projectImg__02} projectLink="сайт-визитка" />
         <div className={styles.project__bottom}>
           <div className={styles.techStack}>
             <article className={styles.techStack__item}>React</article>
@@ -34,8 +35,8 @@ export default function Skills() {
           <div className={styles.project__content}>
             <h3 className={styles.title}>ARLEZU - Изучение армянского</h3>
             <p className={styles.description}>
-              Начинающий веб-разработчик. Владею навыками проектирования
-              интерфейсов, вёрстки и работы с backend.
+              Приложение по изучению армянского языка. Упор сделан на
+              запоминание слов через визуальные образы
             </p>
           </div>
         </div>
